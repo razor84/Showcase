623d38b7d0cc79b7cda4d64182ba313801994252
jereksel <jereksel@gmail.com>
2015-07-25 12:26:49 +0200

    Change resize logic

--- app/src/main/java/com/lovejoy777/showcase/ImageLoadTask.java
+++ app/src/main/java/com/lovejoy777/showcase/ImageLoadTask.java
@@ -1,8 +1,9 @@
 package com.lovejoy777.showcase;
 
-import android.content.Context;
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.AsyncTask;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -12,14 +13,14 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
-    private Context context;
+    private Activity context;
     private String url;
     private ImageView imageView;
 
-    public ImageLoadTask(Context context, String url, ImageView imageView) {
+    public ImageLoadTask(Activity context, String url, ImageView imageView) {
         this.url = url;
         this.imageView = imageView;
-        this.context=context;
+        this.context = context;
     }
 
     @Override
@@ -42,11 +43,21 @@ public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
     @Override
     protected void onPostExecute(Bitmap result) {
         super.onPostExecute(result);
-        if (!(result == null)){
-            imageView.setImageBitmap(result);
-                imageView.setImageBitmap(Bitmap.createScaledBitmap(result, (int) (result.getWidth() * 0.4), (int) (result.getHeight() * 0.4), true));
-        }else{
-            Toast.makeText(context,"There seems to be a problem with the selected Theme", Toast.LENGTH_SHORT).show();
+        if (!(result == null)) {
+
+            Point point = new Point();
+
+            //Get scale (picture height = 1/2 device height)
+            context.getWindowManager().getDefaultDisplay().getRealSize(point);
+
+            int newHeight = point.y / 2;
+
+            float scale = (float) newHeight / (float) result.getHeight();
+
+            //  imageView.setImageBitmap(result);
+            imageView.setImageBitmap(Bitmap.createScaledBitmap(result, (int) (result.getWidth() * scale), (int) (result.getHeight() * scale), true));
+        } else {
+            Toast.makeText(context, "There seems to be a problem with the selected Theme", Toast.LENGTH_SHORT).show();
         }
     }
 }
